55633f34653330417d642ad0359f617746a23060
Zfanz12 <zulfanfalah@gmail.com>
2026-03-16 04:58:49 +0700

    second commit

---
+++ KodePos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class KodePos
+{
+    public string getKodePos(string kelurahan)
+    {
+        Dictionary<string, string> tabel = new Dictionary<string, string>()
+        {
+            { "Batununggal", "40266" },
+            { "Kujangsari",  "40287" },
+            { "Mengger",     "40267" },
+            { "Wates",       "40256" },
+            { "Cijaura",     "40287" },
+            { "Jatisari",    "40286" },
+            { "Margasari",   "40286" },
+            { "Sekejati",    "40286" },
+            { "Kebonwaru",   "40272" },
+            { "Maleer",      "40274" }
+        };
+
+        if (tabel.ContainsKey(kelurahan))
+            return tabel[kelurahan];
+        else
+            return "Kelurahan tidak ditemukan";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+        string[] daftarKelurahan = {
+            "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura",
+            "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer"
+        };
+
+        Console.WriteLine("=== Kode Pos Kelurahan ===");
+        foreach (string kel in daftarKelurahan)
+        {
+            Console.WriteLine($"{kel}: {kp.getKodePos(kel)}");
+        }
+    }
+}
